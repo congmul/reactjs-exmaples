939a93bd7e76706765b396d534b19001866a3bb8
Jehyun <congmul@hotmail.com>
2022-06-23 13:43:43 -0700

    Update comment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders REACT STARTKIT', () => {
+  // 1) Rendering the component we want to test on Virtual DOM
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // 2) finding the elements
+  const linkElement = screen.getByText(/REACT STARTKIT/i);
+  // 3) Assertion
   expect(linkElement).toBeInTheDocument();
 });
