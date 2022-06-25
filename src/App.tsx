be1bcc95c93729ec448d0c808ee4bc4b0dee9b74
Jehyun <congmul@hotmail.com>
2022-06-25 09:29:22 -0700

    Add sign-up

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import Intro from "./pages/Intro/Intro";
+import Signup from "./components/Signup/Signup";
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
 
@@ -10,6 +11,7 @@ const App = ():JSX.Element => {
     <Routes>
       <Route path="/" element={<Layout />}> {/* Wrapping whole pages in the layout */}
         <Route path="/intro" element={<Intro />} />
+        <Route path="/sign" element={<Signup />} />
         <Route path="/about" element={<About />} />
         <Route path="/home" element={<Home />} />
       </Route>
